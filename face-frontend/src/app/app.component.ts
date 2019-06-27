975d3aacfedace6730804c1e28c6d90ed515d571
MarcoMulato <marco.antonio.mulato@gmail.com>
2019-06-26 22:45:48 -0500

    Creacion de captura de fotos

--- face-frontend/src/app/app.component.ts
+++ face-frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireStorage } from '@angular/fire/storage';
 import { finalize, delay } from 'rxjs/operators';
@@ -16,19 +16,65 @@ export class AppComponent {
   show:boolean=false;
   showPB:boolean=false;
   lista:any;
+  nombre:string="";
+  @ViewChild("video",{ static:false})
+    public video: ElementRef;
+
+    @ViewChild("canvas",{ static:false})
+    public canvas: ElementRef;
+
+    public captures: Array<any>;
   
   constructor(
     private firestorage: AngularFirestore,
-    private storage: AngularFireStorage) {}
+    private storage: AngularFireStorage) { this.captures = [];}
+    public ngAfterViewInit() {
+      if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+          navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+              this.video.nativeElement.srcObject = stream;
+              this.video.nativeElement.play();
+          });
+      }
+  }
+
+  public capture() {
+      var context = this.canvas.nativeElement.getContext("2d").drawImage(this.video.nativeElement, 0, 0, 640, 480);
+      this.captures.push(this.canvas.nativeElement.toDataURL("image/png"));
+      const capture = this.canvas.nativeElement.toDataURL("image/png")
+      this.showPB=true;
+      this.nombre = "Imagen-"+Date.now()
+      
+      this.nombre = "Imagen-"+Date.now()+".jpg"
+      const fileRef = this.storage.ref(this.nombre);
+      let fileId =  this.nombre.split('.jpg')[0];
+      const task = this.storage.ref(this.nombre).putString(capture,"data_url")
+      // observe percentage changes
+    this.uploadPercent = task.percentageChanges();
+    // get notified when the download URL is available
+    task.snapshotChanges().pipe(finalize(() => this.downloadURL = fileRef.getDownloadURL())).subscribe();
+
+    // get collection item from file
+    let docRef = this.firestorage.collection("photos").doc(fileId);
+
+    docRef.snapshotChanges().subscribe(res => {
+      //console.log(res);
+      //console.log(res.payload);
+      //console.log(res.payload.data());
+      this.lista=res.payload.data()
+    });
+    task.percentageChanges().pipe(finalize(() =>this.show=true)).subscribe();
+  }
 
   uploadFile(event:any) {
+      //console.log("El evento: ", event)
     this.showPB=true;
-    console.log("DFAWCAWERCWERCAWER: ", this.uploadPercent)
-    console.log(event)
+    //console.log("DFAWCAWERCWERCAWER: ", this.uploadPercent)
+    //console.log(event)
     const file = event.target.files[0];
-    console.log(file);
+    ////console.log(file);
 
     const filePath = file.name;
+    //console.log(filePath);
     this.nombreSubida = file.name;
     let fileId;
     if(filePath.includes('.jpg')){
@@ -48,6 +94,7 @@ export class AppComponent {
     }
 
     const fileRef = this.storage.ref(filePath);
+    //console.log("LA NUEVA WEA: ", fileRef)
     const task =  this.storage.upload(filePath, file);
 
     // observe percentage changes
@@ -59,9 +106,9 @@ export class AppComponent {
     let docRef = this.firestorage.collection("photos").doc(fileId);
 
     docRef.snapshotChanges().subscribe(res => {
-      console.log(res);
-      console.log(res.payload);
-      console.log(res.payload.data());
+      //console.log(res);
+      //console.log(res.payload);
+      //console.log(res.payload.data());
       this.lista=res.payload.data()
     });
     task.percentageChanges().pipe(finalize(() =>this.show=true)).subscribe();
